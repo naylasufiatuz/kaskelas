e1cd0ded02f998ce286ac8e38367880dcb2d61ac
naylasufiatuz <nilaaaminie3bite@gmail.com>
2026-08-02 17:27:29 +0700

    Recreate storage directories at container startup

--- docker/start.sh
+++ docker/start.sh
@@ -7,6 +7,11 @@ sed -i "s/80/${PORT}/g" /etc/apache2/ports.conf /etc/apache2/sites-available/000
 
 cd /var/www/html
 
+# Make sure Laravel's writable directories exist every time the container starts -
+# a previous deploy crashed here because storage/framework/views didn't exist yet.
+mkdir -p storage/framework/cache/data storage/framework/sessions storage/framework/testing storage/framework/views storage/logs bootstrap/cache
+chown -R www-data:www-data storage bootstrap/cache
+
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
